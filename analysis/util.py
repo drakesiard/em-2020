e0d731e1a48e8696ea26377829df4749a4c072be
Drake Siard <drake.siard@gmail.com>
2020-09-21 09:13:13 +0100

    Larger universe, tidy up plots

--- analysis/util.py
+++ analysis/util.py
@@ -12,13 +12,14 @@ PLOT_DIR = '../tex/plots/dis'
 
 RAW_DATASET = os.path.join(DATA_DIR, 'combined_dataset_raw.csv')
 DATASET_SUBSET = os.path.join(DATA_DIR, 'combined_dataset_subset.csv')
-DATASET_FINAL = os.path.join(DATA_DIR, 'combined_dataset_final.csv')
+DATASET_SUBSET_FINAL = os.path.join(DATA_DIR, 'combined_dataset_subset_final.csv')
+DATASET_ALL = os.path.join(DATA_DIR, 'combined_dataset_all.csv')
+DATASET_ALL_FINAL = os.path.join(DATA_DIR, 'combined_dataset_all_final.csv')
 
 RAFIQ2016_COUNTRIES = ['AGO', 'BGD', 'CHN', 'CRI', 'ETH', 'GHA', 'IND', 'IDN', 'JOR', 'LBN', 'MYS', 'MNG', 'MOZ', 'NAM', 'NGA', 'PAN', 'SGP', 'SDN', 'TZA', 'THA', 'VNM', 'ZMB']
 TIBA2018_MID_COUNTRIES = ['DZA', 'ARG', 'BRA', 'BGR', 'CHL', 'CHN', 'COL', 'MYS', 'MEX', 'THA', 'TUR', 'VEN']
 TIBA2018_HIGH_COUNTRIES = ['AUS', 'CAN', 'FRA', 'DEU', 'JPN', 'NLD', 'PRT', 'ESP', 'SWE', 'CHE', 'GBR', 'USA']
-
-ALL_COUNTRIES = set(TIBA2018_MID_COUNTRIES + TIBA2018_HIGH_COUNTRIES)
+ALL_COUNTRIES = ['ALB', 'ARG', 'ARM', 'AUS', 'AUT', 'AZE', 'BEL', 'BGD', 'BGR', 'BLR', 'BRA', 'CAN', 'CHE', 'CHL', 'CHN', 'COL', 'CYP', 'CZE', 'DEU', 'DNK', 'DOM', 'DZA', 'ECU', 'EGY', 'ESP', 'EST', 'FIN', 'FRA', 'GBR', 'GEO', 'GRC', 'GTM', 'HKG', 'HND', 'HRV', 'HUN', 'IDN', 'IND', 'IRL', 'IRN', 'ISL', 'ISR', 'ITA', 'JAM', 'JOR', 'JPN', 'KAZ', 'KEN', 'KGZ', 'KOR', 'LKA', 'LTU', 'LVA', 'MAR', 'MDA', 'MEX', 'MKD', 'MLT', 'MOZ', 'MYS', 'NLD', 'NOR', 'NZL', 'PAK', 'PAN', 'PER', 'PHL', 'POL', 'PRT', 'PRY', 'ROU', 'RUS', 'SAU', 'SGP', 'SVK', 'SVN', 'SWE', 'THA', 'TJK', 'TUN', 'TUR', 'TZA', 'UKR', 'URY', 'USA', 'UZB', 'VEN', 'VNM', 'ZAF', 'ZMB']
 
 def save_table(table, file, save=False, **kwargs):
     if save:
